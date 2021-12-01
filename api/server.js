5b4b31b1aa7296f1265d7e2da8c4eb042c730ea8
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-01 13:09:21 -0800

    connected user router to the server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,17 @@
+// ===== IMPORTS =====
 const express = require('express');
-
-const server = express();
+const usersRouter = require("./users/users-router")
 
 // remember express by default cannot parse JSON in request bodies
+// ===== INSTANCE OF EXPRESS =====
+const server = express();
 
 // global middlewares and the user's router need to be connected here
+// ===== GLOBAL MIDDLEWARE =====
+server.use(express.json())
+
+// ===== ROUTES =====
+server.use("/api/users", usersRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);

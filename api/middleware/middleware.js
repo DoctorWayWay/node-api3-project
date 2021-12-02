b36aa46ce30af9b896590147e928a91210e7ef7b
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-01 16:47:12 -0800

    added a update user EP

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -32,6 +32,7 @@ function validateUser(req, res, next) {
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  
 }
 
 // do not forget to expose these functions to other modules
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -49,10 +49,17 @@ router.post('/', validateUser, async (req, res, next) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, async (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  try {
+    const { id } = req.params
+    const updatedUser = await Users.update(id, req.body)
+    res.status(200).json(updatedUser)
+  } catch (error) {
+    next(error)
+  }
 });
 
 router.delete('/:id', (req, res) => {
